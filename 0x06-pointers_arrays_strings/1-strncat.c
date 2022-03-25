b7ed628b6a7cee9815232f813620c111d2ac50f3
DEMBELE Modibo <dembelemod12@gmail.com>
2022-03-25 00:15:37 +0000

    concatenates two string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,22 @@
 #include "holberton.h"
 /**
- *_strncat -  concatenates two strings
- *@dest: A pointer to an char that will be updated
- *@src: A pointer to an char that will be updated
- *@n: value
- *Return: dest
+ * _strncat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: number of bytes to print
+ * Return: pointer to the resulting string dest
  */
-
 char *_strncat(char *dest, char *src, int n)
-
 {
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0') /*WHILE count character */
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0' && i < n)
 	{
+		dest[j + i] = src[i];
 		i++;
 	}
-
-	j = 0;
-	while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
-	{
-		dest[i] = src[j];
-		j++;  /*sum j*/
-		i++; /**sum i*/
-	}
-
-	dest[i] = '\0'; /*the terminating null byte*/
-
 	return (dest);
 }
